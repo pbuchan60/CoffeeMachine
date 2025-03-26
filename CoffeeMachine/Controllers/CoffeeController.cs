a7989f1d34c18172239f5f51720be84c703d69fb
pbuch <pbuch@PETESASUS>
2025-03-26 12:23:07 +1100

    Add project files.

---
+++ CoffeeMachine/Controllers/CoffeeController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Runtime.InteropServices.JavaScript;
+using CoffeeMachine.Middleware;
+using CoffeeMachine.Models;
+
+namespace CoffeeMachine.Controllers
+{
+    [ApiController]
+    [Route("api/brew-coffee")]
+    public class CoffeeController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            int count = RequestCountMiddleware.GetEndpointRequestCount("/api/brew-coffee");
+            if (count == 5)
+            {
+                return StatusCode(503, "Service Unavailable");
+            }
+            else
+            {
+                if (DateTime.Now.Month == 4 && DateTime.Now.Day == 1)
+                {
+                    return StatusCode(418, "I'm a teapot");
+                }
+                else
+                {
+                    BrewDto brew = new BrewDto();
+                    brew.Message = "Your piping hot coffee is ready";
+                    brew.Prepared = DateTime.Now;
+                    return new JsonResult(brew);
+                }
+            } 
+        }
+    }
+}
